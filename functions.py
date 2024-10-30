07041068a8c184a1a24f1258a9d39b4c8149f2ef
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2024-10-30 11:33:26 -0400

    added true math functions to accurately perform basic math on floats. added is_increment(), next_lower_measurable(), and next_higher_measurable()

--- functions.py
+++ functions.py
@@ -1,6 +1,60 @@
+import math
+
 from cyclophosphamide import Cyclophosphamide
 
-def to_measurable(vol):
+def true_divide(x, y):
+    i = 0
+    while x%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    while y%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    result = (x / y) / (10**i)
+    return result
+
+def true_subtraction(x, y):
+    i = 0
+    while x%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    while y%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    result = (x - y) / (10**i)
+    return result
+
+def true_addition(x, y):
+    i = 0
+    while x%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    while y%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    result = (x + y) / (10**i)
+    return result
+
+def true_mod(x, y):
+    i = 0
+    while x%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    while y%1 != 0:
+        x *= 10
+        y *= 10
+        i += 1
+    result = (x % y) 
+    return result
+
+def get_increment(vol):
     increment = 0
     if vol <= 3:
         increment = 0.05
@@ -8,5 +62,43 @@ def to_measurable(vol):
         increment = 0.1
     else:
         increment = 0.5
-    return (vol//increment)*increment
+    return increment
 
+def is_increment(vol):
+    increment = get_increment(vol)
+    if true_mod(vol, increment) == 0:
+        return True
+    return False
+
+def to_measurable(vol):
+    increment = get_increment(vol)
+    if true_mod(vol, increment) == 0:
+        result = vol
+    else:
+        result = (vol//increment)*increment
+    return result
+
+def next_lower_measurable(vol):
+    if not is_increment(vol):
+        raise Exception("Error: volume must be a measurable increment")
+    increment = get_increment(vol)
+    result = true_subtraction(vol, increment)
+    
+    if not is_increment(result):
+        increment = get_increment(result)
+        result = true_subtraction(vol, increment)
+    
+    return result
+
+def next_higher_measurable(vol):
+    if not is_increment(vol):
+        raise Exception("Error: volume must be a measurable increment")
+    increment = get_increment(vol)
+    result = true_addition(vol, increment)
+    
+    if not is_increment(result):
+        increment = get_increment(result)
+        result = true_addition(vol, increment)
+    
+    return result
+    
--- tests.py
+++ tests.py
@@ -6,3 +6,27 @@ class Tests(unittest.TestCase):
     def test_to_measurable(self):
         self.assertEqual(to_measurable(3.14), 3.1)
         self.assertEqual(to_measurable(25.2), 25)
+        self.assertEqual(to_measurable(4.2), 4.2)
+        self.assertEqual(to_measurable(0.108), 0.1)
+    
+    def test_is_increment(self):
+        self.assertEqual(is_increment(4.2), True)
+        self.assertEqual(is_increment(4.1), True)
+        self.assertEqual(is_increment(3.14), False)
+        self.assertEqual(is_increment(3.1), True)
+
+    def test_next_lower_measureable(self):
+        self.assertEqual(next_lower_measurable(3.1), 3)
+        self.assertEqual(next_lower_measurable(4.2), 4.1)
+        self.assertEqual(next_lower_measurable(10.5), 10)
+        self.assertEqual(next_lower_measurable(23.5), 23)
+    
+    def test_true_mod(self):
+        self.assertEqual(true_mod(3.0, 0.05), 0)
+
+    def test_next_higher_measurable(self):
+        self.assertEqual(next_higher_measurable(3.1), 3.2)
+        self.assertEqual(next_higher_measurable(4.2), 4.3)
+        self.assertEqual(next_higher_measurable(10.5), 11)
+        self.assertEqual(next_higher_measurable(3), 3.1)
+        self.assertEqual(next_higher_measurable(10), 10.5)
