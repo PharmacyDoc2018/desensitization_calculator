3e360448006719a4af0c193283adbdeeebad2e0f
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2024-12-11 10:32:55 -0500

    added carboplatin.py

--- carboplatin.py
+++ carboplatin.py
@@ -1,10 +1,10 @@
-class Cyclophosphamide:
+class Carboplatin:
     
     def name():
-        return "cyclophosphamide"
+        return "carboplatin"
     
     def concentration():
-        return 20
+        return 10
     
     def units():
         return "mg"
@@ -15,11 +15,11 @@ class Cyclophosphamide:
     def dilutions():
         return {
             1: {
-                "initial_conc": 20,
+                "initial_conc": 10,
                 "drug_vol": 1,
                 "diluent": "NS",
                 "diluent_amount": 9,
-                "final_conc": 2
+                "final_conc": 1
             }
         }
     
@@ -30,30 +30,30 @@ class Cyclophosphamide:
         return {
             1: {
                 "dose_modifier": 0.001, # bag_dose = total_dose * dose_modifier
-                "drug_conc_added": 2, # mg/mL
+                "drug_conc_added": 1, # mg/mL
                 "diluent_bag": "NS",
-                "diluent_bag_size": 250, # mL
-                "diluent_bag_total_vol": 275, # mL
+                "diluent_bag_size": 100, # mL
+                "diluent_bag_total_vol": 110, # mL
                 "infusion_time_first_dose": 1, # hour
-                "infusion_time_sub_dose": 0.5 # hour
+                "infusion_time_sub_dose": 1 # hour
             },
             2: {
                 "dose_modifier": 0.01, # bag_dose = total_dose * dose_modifier
-                "drug_conc_added": 2, # mg/mL
+                "drug_conc_added": 1, # mg/mL
                 "diluent_bag": "NS",
-                "diluent_bag_size": 250, # mL
-                "diluent_bag_total_vol": 275, # mL
+                "diluent_bag_size": 100, # mL
+                "diluent_bag_total_vol": 110, # mL
                 "infusion_time_first_dose": 1, # hour
-                "infusion_time_sub_dose": 0.5 # hour
+                "infusion_time_sub_dose": 1 # hour
             },
             3: {
                 "dose_modifier": 0.1, # bag_dose = total_dose * dose_modifier
-                "drug_conc_added": 20, # mg/mL
+                "drug_conc_added": 10, # mg/mL
                 "diluent_bag": "NS",
-                "diluent_bag_size": 250, # mL
-                "diluent_bag_total_vol": 275, # mL
+                "diluent_bag_size": 100, # mL
+                "diluent_bag_total_vol": 110, # mL
                 "infusion_time_first_dose": 1, # hour
-                "infusion_time_sub_dose": 0.5 # hour
+                "infusion_time_sub_dose": 1 # hour
             },
             4: {
                 "dose_modifier": 0.889, # bag_dose = total_dose * dose_modifier
@@ -62,6 +62,6 @@ class Cyclophosphamide:
                 "diluent_bag_size": 250, # mL
                 "diluent_bag_total_vol": 275, # mL
                 "infusion_time_first_dose": 1, # hour
-                "infusion_time_sub_dose": 0.5 # hour
+                "infusion_time_sub_dose": 1 # hour
             }
         }
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from cyclophosphamide import Cyclophosphamide
 from oxaliplatin import Oxaliplatin
+from carboplatin import Carboplatin
 from bag import Bag
 from functions import *
 
 def main():
-    print(generate_desensitization_instructions(Oxaliplatin, 150))
+    print(generate_desensitization_instructions(Carboplatin, 625))
 
 main()
